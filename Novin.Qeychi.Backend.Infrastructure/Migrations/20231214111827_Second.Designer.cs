870e435a745f24cf5255f9c1eb55637969b70abf
Mahsa Shahverdi <mahsashahverdi@yahoo.com>
2023-12-14 14:49:40 +0330

    edit relations

--- Novin.Qeychi.Backend.Infrastructure/Migrations/20231214111827_Second.Designer.cs
+++ Novin.Qeychi.Backend.Infrastructure/Migrations/20231214111827_Second.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Novin.Qeychi.Backend.Infrastructure.Database;
 
@@ -11,9 +12,11 @@ using Novin.Qeychi.Backend.Infrastructure.Database;
 namespace Novin.Qeychi.Backend.Infrastructure.Migrations
 {
     [DbContext(typeof(QeychiDB))]
-    partial class QeychiDBModelSnapshot : ModelSnapshot
+    [Migration("20231214111827_Second")]
+    partial class Second
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Novin.Qeychi.Backend.Infrastructure/Migrations/20231214111827_Second.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Novin.Qeychi.Backend.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Second : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Day",
+                table: "ConfirmationEmployments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Time",
+                table: "ConfirmationEmployments",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Day",
+                table: "ConfirmationEmployments");
+
+            migrationBuilder.DropColumn(
+                name: "Time",
+                table: "ConfirmationEmployments");
+        }
+    }
+}
